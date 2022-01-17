de300bd3abdc723cdcdb784d87643eb8a6263df6
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-16 16:21:45 -0800

    added improved addition function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-function add(number1, number2)	{
-	return number1 + number2;
+// Addition Logic
+function add(number1, number2) {
+  return number1 + number2;
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+const result = add(number1, number2);
+alert(result);
