252455c6e82ee553ff7d2c5b7b9f3e2d26db578d
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-16 16:26:36 -0800

    added subtract function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
-// Addition Logic
+// Add Logic
 function add(number1, number2) {
   return number1 + number2;
 }
 
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = add(number1, number2);
+
+// Subtract Logic
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+const result = subtract(number1, number2);
 alert(result);
