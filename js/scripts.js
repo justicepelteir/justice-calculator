8a8e9e65db065e855d4a334638995b1f1715c720
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-16 16:34:13 -0800

    uncommented out calculator functions for testing

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-// const number1 = parseInt(prompt("Enter a number:"));
-// const number2 = parseInt(prompt("Enter another number:"));
-// const result = add(number1, number2);
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const additionResult = add(number1, number2);
 // alert(result);
 
 // Subtract Logic
@@ -13,27 +13,27 @@ function subtract(number1, number2) {
   return number1 - number2;
 }
 
-// const number1 = parseInt(prompt("Enter a number:"));
-// const number2 = parseInt(prompt("Enter another number:"));
-// const result = subtract(number1, number2);
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const subtractionResult = subtract(number1, number2);
 // alert(result);
 
 // Multiply Logic
-// function multiply(number1, number2) {
-//   return number1 * number2;
-// }
+function multiply(number1, number2) {
+  return number1 * number2;
+}
 
-// const number1 = parseInt(prompt("Enter a number:"));
-// const number2 = parseInt(prompt("Enter another number:"));
-// const result = multiply(number1, number2);
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const multiplicationResult = multiply(number1, number2);
 // alert(result);
 
-// Multiply Logic
+// Divide Logic
 function divide(number1, number2) {
   return number1 / number2;
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = divide(number1, number2);
-alert(result);
+const divisionResult = divide(number1, number2);
+// alert(result);
