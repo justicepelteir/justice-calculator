effd80b4d028dce5c2ebd29455718084db337580
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-16 16:30:28 -0800

    added multiply and divide functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,34 @@ function add(number1, number2) {
 // const number1 = parseInt(prompt("Enter a number:"));
 // const number2 = parseInt(prompt("Enter another number:"));
 // const result = add(number1, number2);
+// alert(result);
 
 // Subtract Logic
 function subtract(number1, number2) {
   return number1 - number2;
 }
 
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = subtract(number1, number2);
+// alert(result);
+
+// Multiply Logic
+// function multiply(number1, number2) {
+//   return number1 * number2;
+// }
+
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+// const result = multiply(number1, number2);
+// alert(result);
+
+// Multiply Logic
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = subtract(number1, number2);
+const result = divide(number1, number2);
 alert(result);
